f25b3d5335f5e043ca89b130f2c1e7d2c0afe4ac
igorjosiel <igorjosielvitalino@gmail.com>
2026-07-15 20:50:09 -0300

    feat: cria o método put para atualizar os registros através do id.

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,5 +1,3 @@
-const 
-
 class Controller {
     constructor(entityService) {
         this.entityService = entityService;
@@ -12,6 +10,23 @@ class Controller {
             return res.status(200).json(registersList);
         } catch (error) {
 
+        }
+    }
+
+    async update(req, res) {
+        const { id } = req.params;
+        const updatedData = req.body;
+        
+        try {
+            const wasUpdated = await this.entityService.updateRegister(updatedData, Number(id));
+
+            if (!wasUpdated) {
+                return res.status(400).json({ message: "Registro não foi atualizado." });
+            }
+
+            return res.status(200).json({ message: "Atualizado com sucesso." });
+        } catch (error) {
+            // erro
         }
     }
 }
